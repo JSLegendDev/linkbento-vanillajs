a28c7df5879b9402d7fa5ab552231f256a06801c
Nassim's Software <128189209+JSLegendDev@users.noreply.github.com>
2023-05-25 10:08:11 -0400

    creating the main page

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-import './index.css'
 
+function button(text, onClick) {
+    const domElement = document.createElement('button')
+    domElement.innerText = text
+    domElement.addEventListener('click', onClick)
+    domElement.classList.add(
+        'bg-blue-500',
+        'hover:bg-blue-700',
+        'text-white',
+        'font-bold', 
+        'py-2', 
+        'px-4',
+        'rounded'
+    )
 
-const app = document.getElementById('app')
-app.innerText = 'HELLO WORLD'
+    return domElement
+}
+
+function main() {
+    const app = document.getElementById('app')
+
+    
+    app.appendChild(button('Add Link', () => {
+        console.log('text')
+    }))
+}
+
+main()
