244df5c29353040f51a97812f29daac43b528313
Nassim's Software <128189209+JSLegendDev@users.noreply.github.com>
2023-05-25 10:35:36 -0400

    almost finished link form

--- main.js
+++ main.js
@@ -16,13 +16,59 @@ function button(text, onClick) {
     return domElement
 }
 
+function appendChildren(parent, elements) {
+    for (const element of elements) {
+        parent.appendChild(element)
+    }
+}
+
+function linkForm() {
+    const rootElement = document.createElement('div')
+    rootElement.classList.add(
+        'flex',
+        'flex-col',
+    )
+    const [titleField, urlField, descriptionField] = [
+        document.createElement('input'),
+        document.createElement('input'),
+        document.createElement('input')
+    ]
+
+    const inputStyles = [
+        'bg-gray-200',
+        'shadow',
+        'appearance-none',
+        'border', 
+        'rounded',
+        'w-full', 
+        'py-2',
+        'px-3',
+        'my-3',
+        'text-gray-700',
+        'leading-tight',
+        'focus:outline-none',
+        'focus:shadow-outline'
+    ]
+    titleField.placeholder = 'Enter a title for the link'
+    titleField.classList.add(...inputStyles)
+    urlField.placeholder = 'Enter the url for the link'
+    urlField.classList.add(...inputStyles)
+    descriptionField.placeholder = 'Write a short description for the link'
+    descriptionField.classList.add(...inputStyles)
+    
+    appendChildren(rootElement, [
+        titleField, 
+        urlField, 
+        descriptionField,
+        button('Add link', () => console.log('link added'))
+    ])
+
+    return rootElement
+}
+
 function main() {
     const app = document.getElementById('app')
-
-    
-    app.appendChild(button('Add Link', () => {
-        console.log('text')
-    }))
+    app.appendChild(linkForm())
 }
 
 main()
